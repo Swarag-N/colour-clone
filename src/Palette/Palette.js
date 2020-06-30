673c5fcf14d22afdb5c3d97e7e7282b78547cab7
whois-this <46181010+whois-this@users.noreply.github.com>
2020-06-30 20:00:00 +0530

    added slider

--- src/Palette/Palette.js
+++ src/Palette/Palette.js
@@ -1,22 +1,39 @@
 import React, { Component } from 'react'
 import ColorBox from './ColorBox'
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
+
 import './Palette.css'
 
 class Palette extends Component {
+    constructor(props){
+        super(props)
+        this.state ={
+            level:300
+        }
+        this.handleLevelChange = this.handleLevelChange.bind(this)
+    }
+    handleLevelChange(level){
+        this.setState({level})
+    }
     render() {
-        let {colors,paletteName} = this.props.palette
+        const {colors,paletteName} = this.props.palette
+        const {level} = this.state
         let colourBoxes = []
-        colors[300].every(colour=>(
+        colors[level].every(colour=>(
             colourBoxes.push(<ColorBox background={colour.hex} name={colour.name} key={colour.color}/>)
         ))
+
         return (
             <div className="Palette">
                 <h1>{paletteName}</h1>
                 <div className="Palette-colors">
+                <Slider step={100} dots={true} activeDotStyle={{borderColor: 'yellow'}} min={100} max={900} defaultValue={level}onAfterChange={this.handleLevelChange}/> 
                     {colourBoxes}
                 </div>
             </div>
         )
     }
 }
+
 export default Palette
